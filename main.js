f50a7bddaf0725053312442f37e0ff8d8323f927
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-18 13:28:24 -0800

    Used absolute positioning for car image.#3

--- main.js
+++ main.js
@@ -3,10 +3,13 @@
 var $carImg = document.querySelector('.img-container');
 
 var car = {
-  position: []
+  x:0,
+  y:0,
 };
 addEventListener('keydown', function (event) {
-  console.log($carImg.position);
+  console.log($carImg.style);
+  console.log(event);
+  car.position
   if (event.key == 'ArrowUp') {
     $carImg.className = 'img-container up';
   } else if (event.key == 'ArrowDown') {
@@ -15,5 +18,8 @@ addEventListener('keydown', function (event) {
     $carImg.className = 'img-container right';
   } else if (event.key == 'ArrowLeft') {
     $carImg.className = 'img-container left';
+  } else if (event.key == 'Space') {
+    $carImg.className = 'img-container left';
+    $carImg.style.left =
   }
 });
